33b358237d120f7f402f1c69b20f6f3f524af55f
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2018-02-25 16:01:38 -0500

    Improvements to helper script

--- preprocess_kitti_mnc.sh
+++ preprocess_kitti_mnc.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+# Builds a Docker image for the MNC Caffe and uses it to preprocess the
+# instance-aware semantic segmentations for a KITTI Tracking or Odometry
+# sequence.
+#
+# Note that the script is a little slow, not really because of the inference,
+# but because of inefficient IO.
+#
+# The output files will be owned by the Docker user, which may be root, but
+# should be publicly readable.
+#
 # Credits:
 #   * Nikolaus Mayer from Uni Freiburg for inspiring me to Dockerize my code
 #     after seeing just how amazingly easy it was to work with the dockerized
 #     versions of DispNet an FlowNet developed by him and the vision lab at
 #     the University of Freiburg.
-
-# TODO(andreib): Separate script for downloading and unzipping data. Make this script call if if 'dataset-root' not given.
 
 # TODO-LOW(andreib): Check for minimum driver version since that's something
 # which can't be abstracted by Docker for now.
@@ -14,6 +22,7 @@
 # TODO-LOW(andreib): Makefile to make things even easier.
 
 set -eu
+GPU_ID_TO_USE=0
 
 usage() {
     cat >&2 <<EOF
@@ -32,6 +41,7 @@ Example:
 EOF
 }
 
+# TODO(andreib): Move to common helper.
 fail() {
   ret="$?"
   # echo first argument in red
@@ -66,26 +76,26 @@ SEQUENCE_ID="$4"
 
 # TODO(andrei): Support both tracking and odometry.
 
-# TODO pass kitti dir location to image as mount
 nvidia-docker build -t mnc-docker-img . || fail "Could not build Docker image."
-nvidia-docker run -t mnc-docker-img \
-    bash -c \
-    'cd /opt/mnc/caffe-mnc && ls -lah && nvidia-smi' ||
-    fail "Docker sanity check failed"
+#nvidia-docker run -t mnc-docker-img \
+#    bash -c \
+#    'cd /opt/mnc/caffe-mnc && ls -lah && nvidia-smi' ||
+#    fail "Docker sanity check failed"
 
 
 #for id in $(seq -f '%04g' 0 20); do
 id="$(printf '%04d' $SEQUENCE_ID)"
-printf "\n\t%s\n\n" "Processing sequence [$id]..."
+printf "\n\t%s\n\n" "Processing [$DATASET] [$SPLIT] sequence [$id]..."
 
-SEQUENCE_IN="training/image_02/$id"
-SEQUENCE_OUT="training/seg_image_02/$id"
+SEQUENCE_IN="$SPLIT/image_02/$id"
+SEQUENCE_OUT="$SPLIT/seg_image_02/$id/mnc"
 
 # TODO(andrei): Check if SEQUENCE_OUT exists and has first and last segmented frames and skip.
 
 # Note: the tool is called 'demo.py' for historical reasons.
 nvidia-docker run \
     --mount src="$DATASET_ROOT",target=/data/kitti,type=bind \
+    -e CUDA_VISIBLE_DEVICES="$GPU_ID_TO_USE" \
     -t mnc-docker-img \
     bash -c "cd /opt/mnc && mkdir -p /data/kitti/$SEQUENCE_OUT && CUDA_VISIBLE_DEVICES=0 tools/demo.py --input /data/kitti/$SEQUENCE_IN --output /data/kitti/$SEQUENCE_OUT"
 #done
